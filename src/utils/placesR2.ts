bccd1d346010f933a1c1703f80ed4c062cbe42be
김민경 <105103167+sparrowscout@users.noreply.github.com>
2026-05-06 17:51:32 +0900

    fix: 환경변수 통일

--- src/utils/placesR2.ts
+++ src/utils/placesR2.ts
@@ -2,7 +2,7 @@ import { S3Client } from '@aws-sdk/client-s3';
 
 export const placesR2 = new S3Client({
   region: 'auto',
-  endpoint: process.env.R2_PLACES_ENDPOINT!,
+  endpoint: process.env.R2_ENDPOINT!,
   forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.R2_S3_ACCESS_KEY_ID!,
